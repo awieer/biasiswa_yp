92f3c5d0e86175a3baced97bc2b238f5ab6de8c9
Atikah <68261675+atikahnann@users.noreply.github.com>
2020-10-27 02:00:14 +0800

    student detail page
    student detail page

---
+++ lib/theme/style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class YpTheme {
+  static const headingText = TextStyle(
+    color: Colors.black,
+    fontSize: 28,
+  );
+  static const bodyText = TextStyle(
+    color: Colors.black,
+    fontSize: 14,
+  );
+  static const bodyH3Text = TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+  );
+
+  static const bodyH5Text = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+}
